6fecd5b66c66cf6ba76637aa5c8a985f73759182
Viktor Yershov <viktor@spice.ai>
2026-02-25 18:24:40 -0800

    Fix

--- crates/adbc_client/src/lib.rs
+++ crates/adbc_client/src/lib.rs
@@ -14,9 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-pub mod databricks;
-pub mod spiceai;
-
 pub use adbc_core::options::IngestMode;
 
 use adbc_core::options::{self, AdbcVersion, OptionDatabase, OptionValue};
