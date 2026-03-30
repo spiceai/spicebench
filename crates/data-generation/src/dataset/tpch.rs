4e49383483ed4eb86de565bbad6c8ecbb08a7ddc
Jack Eadie <jack@spice.ai>
2026-03-29 19:09:39 -0700

    try buckets=2

--- crates/data-generation/src/dataset/tpch.rs
+++ crates/data-generation/src/dataset/tpch.rs
@@ -441,13 +441,13 @@ impl Dataset for TpchDataset {
 
     fn partition_columns(&self, table: &str) -> Vec<String> {
         match table {
-            "lineitem" => vec!["bucket(4, l_linenumber)".to_string()],
-            "orders" => vec!["bucket(4, o_orderkey)".to_string()],
-            "partsupp" => vec!["bucket(4, ps_partkey)".to_string()],
-            "part" => vec!["bucket(4, p_partkey)".to_string()],
-            "supplier" => vec!["bucket(4, s_suppkey)".to_string()],
-            "customer" => vec!["bucket(4, c_custkey)".to_string()],
-            "nation" => vec!["bucket(4, n_nationkey)".to_string()],
+            "lineitem" => vec!["bucket(2, l_linenumber)".to_string()],
+            "orders" => vec!["bucket(2, o_orderkey)".to_string()],
+            "partsupp" => vec!["bucket(2, ps_partkey)".to_string()],
+            "part" => vec!["bucket(2, p_partkey)".to_string()],
+            "supplier" => vec!["bucket(2, s_suppkey)".to_string()],
+            "customer" => vec!["bucket(2, c_custkey)".to_string()],
+            "nation" => vec!["bucket(2, n_nationkey)".to_string()],
             "region" => vec!["r_regionkey".to_string()],
             _ => vec![],
         }
