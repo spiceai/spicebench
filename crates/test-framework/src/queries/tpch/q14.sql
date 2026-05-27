c61c22114621f0dbecb95db31c69b8bb076e3d1a
Viktor Yershov <viktor@spice.ai>
2026-05-27 16:33:37 -0700

    Fix

--- crates/test-framework/src/queries/tpch/q14.sql
+++ crates/test-framework/src/queries/tpch/q14.sql
@@ -5,8 +5,8 @@ select
                              else 0
             end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue
 from
-    sb_66e109_.lineitem,
-    sb_66e109_.part
+    lineitem,
+    part
 where
         l_partkey = p_partkey
   and l_shipdate >= date '1995-09-01'
