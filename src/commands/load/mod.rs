140a24f8579acfb5ef6a792a1a0567f9f90684eb
Viktor Yershov <viktor@spice.ai>
2026-06-08 13:21:39 -0700

    Add run_tag

--- src/commands/load/mod.rs
+++ src/commands/load/mod.rs
@@ -864,6 +864,7 @@ pub(crate) async fn run(
             KeyValue::new("service.name", "spicebench"),
             KeyValue::new("type", "spicebench"),
             KeyValue::new("adapter_name", common_args.system_adapter_name.clone()),
+            KeyValue::new("run_tag", common_args.run_tag.clone()),
             KeyValue::new("scenario", scenario.to_string()),
             KeyValue::new(
                 "data_gen_version",
